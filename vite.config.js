cc94de20570184cb2db58f1b88ab41ecda7512da
Krishnendu Tripathy <krishnendutripathy337@gmail.com>
2023-09-15 09:22:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/webkrish/react-project/"
+  base: "/react-project/"
 })
